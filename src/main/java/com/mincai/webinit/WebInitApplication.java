0d6c144408a8d397b4698b4dffb510721a0f3f0f
limincai <limincai@aliyun.com>
2023-06-03 16:44:01 +0800

    Initial commit

---
+++ src/main/java/com/mincai/webinit/WebInitApplication.java
@@ -0,0 +1,13 @@
+package com.mincai.webinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebInitApplication.class, args);
+    }
+
+}
